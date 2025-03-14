0924c6f44408c2e09f28122389e9789214d4382b
codesungrape <miss.s.rai@outlook.com>
2025-03-14 22:38:43 +0000

    feat: added Navbar component with css.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,14 @@
 //import AuthSignInPage from "./signin/page";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
+import Navbar from "@/components/Navbar/Navbar";
 
 export default function Home() {
   // return <AuthSignInPage />;
   return (
     <>
       <Header />
+      <Navbar />
       <Footer />
     </>
   );
