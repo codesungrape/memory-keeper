7272bc68e1893bcf28220e1a0241c896f6a29313
codesungrape <miss.s.rai@outlook.com>
2025-03-14 23:30:18 +0000

    feat: added Dashboard component for landing page, moved layout specific components to layout.tsx and also reinstated entry point page.tsx to be AuthSignInPage as before.

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Welcome to Memory Keeper</h1>
+      <p>
+        Your memories are stored safely here, ready for your recollection when
+        you need!
+      </p>
+      {/* Need to add other Dashboard-specific content */}
+    </div>
+  );
+}
